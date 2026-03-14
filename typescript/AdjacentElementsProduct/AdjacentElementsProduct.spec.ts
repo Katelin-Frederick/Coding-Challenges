7e344ec941340c5d662f9f7d9e7e23a03a0de145
Katelin Frederick <k.frederick94@gmail.com>
2026-03-13 21:46:09 -0400

    Add Adjacent Elements Challenge

---
+++ typescript/AdjacentElementsProduct/AdjacentElementsProduct.spec.ts
@@ -0,0 +1,11 @@
+import adjacentElementsProduct from './AdjacentElementsProduct';
+
+describe('adjacentElementsProduct', () => {
+    it.each([
+        [[3, 6, -2, -5, 7, 3], 21],
+        [[6, 8, 1, 7, 6, 5, 2], 48],
+        [[9, 4, 3, 2, 8, 7, 3], 56],
+    ] as Array<[number[], number]>)('case %#: returns the largest product of adjacent elements', (input, expected) => {
+        expect(adjacentElementsProduct(input)).toBe(expected);
+    });
+});
---
+++ typescript/AdjacentElementsProduct/README.md
@@ -0,0 +1,25 @@
+# Adjacent Elements Product
+
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+###### Example 1:
+
+```
+Input: [3, 6, -2, -5, 7, 3]
+Output: 21
+Explanation: 7 and 3 produce the largest product
+```
+
+###### Example 2:
+
+```
+Input: [6, 8, 1, 7, 6, 5, 2]
+Output: 48
+```
+
+###### Example 3:
+
+```
+Input: [9, 4, 3, 2, 8, 7, 3]
+Output: 56
+```
